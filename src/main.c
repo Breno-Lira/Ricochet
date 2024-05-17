6d0af443a47f661867749ae2d7a5f849393d194a
Breno-Lira <bmrl@cesar.school>
2024-05-17 15:33:40 -0300

    Adicionando tela inicial e de game over.

--- src/main.c
+++ src/main.c
@@ -1,24 +1,36 @@
-/**
- * main.h
- * Created on Aug, 23th 2023
- * Author: Tiago Barros
- * Based on "From C to C++ course - 2002"
-*/
-
+#include <stdbool.h>
 #include <string.h>
 #include <time.h>
 #include "screen.h"
 #include "keyboard.h"
 #include "timer.h"
 
-int BAR_MIN_X = 35;
-int BAR_MAX_X = 45;
+int BAR_MIN_X = 36;
+int BAR_MAX_X = 46;
 int BAR_Y = MAXY - 1;
 
-int x = 34, y = 12;
+int x = 40, y = 21;
 int incX = 1, incY = 1;
 
-// a = 97 e d = 100
+bool game_over = false;
+
+void telaInicial() {
+    screenGotoxy(37, 10);
+    screenSetColor(WHITE, DARKGRAY);
+    printf("RICOCHET");
+    screenGotoxy(27, 11);
+    printf("Pressione ESPACO para inicar!");
+    screenUpdate();
+
+    while (1) {
+        if (keyhit()) {
+            int ch = readch();
+            if (ch == ' ') {
+                break;
+            }
+        }
+    }
+}
 
 void printBall(int nextX, int nextY)
 {
@@ -94,29 +106,7 @@ void printBlocos(){
         y++;
     }
 
-
-
 }
-/*
- void printKey(int ch)
- {
-     screenSetColor(YELLOW, DARKGRAY);
-     screenGotoxy(35, 22);
-     printf("Key code :");
-
-     screenGotoxy(34, 23);
-     printf("            ");
-
-     if (ch == 27) screenGotoxy(36, 23);
-     else screenGotoxy(39, 23);
-
-     printf("%d ", ch);
-     while (keyhit())
-     {
-         printf("%d ", readch());
-     }
- }
- */
 
 int main() 
 {
@@ -125,55 +115,57 @@ int main()
     screenInit(1);
     keyboardInit();
     timerInit(50);
+    screenGotoxy(x, 22);
+    printf("🔴");
+    printBarra(ch);
+    telaInicial();
+    
+
+    screenInit(1);
+    keyboardInit();
+    timerInit(50);
 
     printBall(x, y);
     printBlocos();
     screenUpdate();
+    printBarra(ch);
 
-    while (ch != 10) //enter
+    while (true) // Loop infinito
     {
-         //Handle user input
-         /*if (keyhit()) 
-         {
-             ch = readch();
-             printKey(ch);
-             screenUpdate();
-         }*/
+        if (!game_over) 
+        {
+            if (timerTimeOver() == 1){
+                
+                int newX = x + incX;
+                if (newX >= (MAXX -strlen("🔴")-1) || newX <= MINX+1) incX = -incX;
+                int newY = y + incY;
+                if (newY >= MAXY-1 || newY <= MINY+1) incY = -incY;
 
-        // Update game state (move elements, verify collision, etc)
-
-        if (timerTimeOver() == 1){
-            
-            int newX = x + incX;
-            if (newX >= (MAXX -strlen("🔴")-1) || newX <= MINX+1) incX = -incX;
-            int newY = y + incY;
-            if (newY >= MAXY-1 || newY <= MINY+1) incY = -incY;
-
-        
-            if (newY == BAR_Y-1 && (newX >= BAR_MIN_X-2 && newX <= BAR_MAX_X+2)) {
-                incY = -incY;  // Inverte a direção vertical da bola
-            }
-
-             
-            //printKey(ch);
-            printBall(newX, newY);
+                if (newY == BAR_Y-1 && (newX >= BAR_MIN_X-2 && newX <= BAR_MAX_X+2)) {
+                    incY = -incY;
+                }
+                
+                printBall(newX, newY);
 
                 if (keyhit()) {
-                ch = readch();
-                printBarra(ch);
+                    ch = readch();
+                    printBarra(ch);
+                    screenUpdate();
+                }
+
+                if(y >= 23){
+                    screenGotoxy(35, 10);
+                    printf("FIM DE JOGO");
+                    game_over = true;
+                }
+
                 screenUpdate();
             }
-
-
-            if(y >= 23){
-                screenGotoxy(35, 10);
-                printf("FIM DE JOGO");
-                
-            }
-
-            
-
-
+        }
+        else // Se game_over for verdadeiro
+        {
+            // Mantenha o loop rodando para manter a tela congelada
+            // mas não execute a lógica de atualização do jogo
             screenUpdate();
         }
     }
@@ -184,7 +176,3 @@ int main()
 
     return 0;
 }
-
- 
-
- 
